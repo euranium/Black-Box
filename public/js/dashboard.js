6bfd5d72daae1f835b9e0a6a412101117c05c08a
Jesse Sliter <sliterj@sliterj-macbookpro.roam.corp.google.com>
2016-03-15 11:42:00 -0700

    messing with angular html injection

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,12 +5,22 @@ app.controller('MainCtrl', [
 '$http',
 function($scope, $http){
   $scope.test = 'Hello world!';
-  $scope.software = []
+  $scope.software = [];
+  $scope.content = "<h1>Dashboard</h1><p>This area will be populated with data from the menu item selected on the left</p>";
 
     $http.get('/api/listsoftware').success(function(data){
       angular.copy(data, $scope.software);
     });
 
+    $scope.loadSoftware = function(name){
+
+      $http.get('/api/template/query?name=' + name).success(function(data){
+        angular.copy(data, $scope.content);
+      });
+
+      console.log($scope.content);
+
+    };
 
 
 
