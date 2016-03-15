89aa5d4e2e113986d1e6518132fce336328143a0
euranium <mountrainier13@gmail.com>
2016-03-15 12:50:01 -0700

    fixed template sending

--- api.go
+++ api.go
@@ -26,9 +26,18 @@ func APIListSoftware(w http.ResponseWriter, r *http.Request) {
 }
 
 func APITemplate(w http.ResponseWriter, r *http.Request) {
-	person, err := IsLoggedIn(w, r)
 	// get folder name
-	p := path.Join(progDir, person.hash)
+	u := r.URL.Query()
+	if len(u["name"]) <= 0 {
+		w.Write([]byte("No Query"))
+		return
+	}
+	q := u["name"][0]
+	if q == "" {
+		w.Write([]byte(""))
+		return
+	}
+	p := path.Join(progDir, q, q+".tmpl")
 	if !CheckFile(p) {
 		w.Write([]byte("No File found"))
 		return
@@ -42,7 +51,7 @@ func APITemplate(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func APIlistresults(w http.ResponseWriter, r *http.Request) {
+func APIListResults(w http.ResponseWriter, r *http.Request) {
 	person, err := IsLoggedIn(w, r)
 	if err != nil {
 		w.Write([]byte(fmt.Sprintf("Error: %s\n", err.Error())))
@@ -64,7 +73,7 @@ func APIlistresults(w http.ResponseWriter, r *http.Request) {
 }
 
 // hard coded results page right now
-func APIResults(w http.ResponseWriter, r *http.Request) {
+func APIGetResults(w http.ResponseWriter, r *http.Request) {
 	person, err := IsLoggedIn(w, r)
 	if err != nil {
 		w.Write([]byte(fmt.Sprintf("Error: %s\n", err.Error())))
