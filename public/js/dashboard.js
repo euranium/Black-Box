b3ee25d7ceedeb196e813c9b624ad2b5f3b84bef
Jesse Sliter <sliterj@sliterj-macbookpro.roam.corp.google.com>
2016-03-15 12:34:43 -0700

    added html injection

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,12 +1,13 @@
-var app = angular.module('dashboard', []);
+var app = angular.module('dashboard', ['ngSanitize']);
 
 app.controller('MainCtrl', [
 '$scope',
 '$http',
-function($scope, $http){
+'$compile',
+'$sce',
+function($scope, $http, $compile, $sce){
   $scope.test = 'Hello world!';
   $scope.software = [];
-  $scope.content = "<h1>Dashboard</h1><p>This area will be populated with data from the menu item selected on the left</p>";
 
     $http.get('/api/listsoftware').success(function(data){
       angular.copy(data, $scope.software);
@@ -15,10 +16,15 @@ function($scope, $http){
     $scope.loadSoftware = function(name){
 
       $http.get('/api/template/query?name=' + name).success(function(data){
-        angular.copy(data, $scope.content);
+        console.log(data);
+        $('#dash').html($compile(data)($scope))//$sce.trustAsHtml(data);
       });
 
-      console.log($scope.content);
+    $scope.send= function(){
+      console.log("Hello");
+    }
+
+      //console.log($scope.content);
 
     };
 
