3d4cd54c7ac14852f6d7a2c5e8ec2227fdc4a7af
Jesse Sliter <sliterj@sliterj-macbookpro.roam.corp.google.com>
2016-03-15 12:56:44 -0700

    hardcoded the dashboard results list

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -26,14 +26,14 @@ function($scope, $http, $compile, $sce){
     };
 
     $scope.send= function(){
-      $scope.results.append($scope.radio);
+      $scope.results.push($scope.radio);
     }
 
     $scope.loadResult = function(name){
 
       $http.get('/api/template/query?name=' + name).success(function(data){
         console.log(data);
-        $('#dash').html($compile(data)($scope));//$sce.trustAsHtml(data);
+        $('#dash').html(name);//$sce.trustAsHtml(data);
       });
 
 
