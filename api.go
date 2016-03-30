99fd4cede2d13d692317ba3323641f6d5c33f93f
euranium <mountrainier13@gmail.com>
2016-03-29 17:03:48 -0700

    adding api form stuff

--- api.go
+++ api.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	//"os/exec"
 	"path"
 )
 
@@ -49,6 +50,58 @@ func APITemplate(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Write([]byte(file))
 	return
+}
+
+func APISubmitForm(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	for k, v := range r.Form {
+		fmt.Println("k:", k, "v:", v)
+	}
+	person, err := IsLoggedIn(w, r)
+	if err != nil || person.user_name == "" {
+		return
+	}
+	// get program name
+	u := r.URL.Query()
+	_, folder := path.Split(r.URL.Path)
+	if folder != "sampleProgs" {
+		w.Write([]byte(""))
+		return
+	}
+	if len(u["name"]) <= 0 {
+		w.Write([]byte("No Query"))
+		return
+	}
+	q := u["name"][0]
+	if q == "" || !IsExec(q) {
+		w.Write([]byte(""))
+		return
+	}
+	if (len(u["type"])) <= 0 {
+		w.Write([]byte("No Type"))
+		return
+	}
+	typ := u["type"][0]
+	dir := path.Join(UserDir, person.hash, RandomString(12))
+	fmt.Println("copying to:", dir)
+	//err = CopyDir(path.Join("executables", q), dir)
+	if err != nil {
+		fmt.Println("error:", err.Error())
+		w.Write([]byte("Error processing\n"))
+	}
+	var args []string
+	if typ == "java" {
+		args = []string{"-classpath", path.Join(dir, q)}
+		for k, _ := range r.Form {
+			args = append(args, k)
+		}
+		fmt.Println(args)
+		//Tasks <- exec.Command("java", args...)
+		w.Write([]byte("submited form\n"))
+		return
+	} else {
+		w.Write([]byte("Only handling java right now"))
+	}
 }
 
 func APIListResults(w http.ResponseWriter, r *http.Request) {
